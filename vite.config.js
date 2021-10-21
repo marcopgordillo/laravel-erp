259d35983431efcdcb20169808fa783ff1969dcd
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2021-10-21 02:01:26 -0400

    Add tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
     outDir: '../public/client',
     emptyOutDir: true,
   },
-  root: './client'
+  root: './client',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './client/src'),
+    },
+  }
 })
